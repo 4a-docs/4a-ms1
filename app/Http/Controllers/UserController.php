0cd7f1066db83d7038bb3bc4d3f8b46272c6bba2
Anderson DZ <anderson9daza@gmail.com>
2021-12-10 22:52:25 -0500

    add element in data return get token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
         }
         $token = JWTAuth::parseToken()->authenticate();
         $user = User::where('id', '=', $token['id'])->get();
-        $role = $user->roles;
+        $role = $user->role;
         // $token->append(['role'=> $user->roles[0]['name']]);
         return response()->json($role, 200);
         // return response()->json($token, 200);
